17caf5563a7d9f8b7684979b4ada559d34fbc592
caniksea <caniksea@yahoo.co.nz>
2021-04-14 19:05:55 +0200

    Added mine

---
+++ src/test/java/za/ac/cput/AccountTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test void testEqual() {
+        assertEquals(1, 1);
+    }
+
+}
